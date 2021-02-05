e490714c15917ce85e52d34db830e62e89969a2c
Mahfuzur Rahman <imahfuzurx@gmail.com>
2021-02-05 12:57:24 +0600

    last update

---
+++ data/ChatRooms.ts
@@ -0,0 +1,145 @@
+export default [{
+  id: '1',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }, {
+    id: 'u2',
+    name: 'Lukas',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p200x200/83620704_175970293764724_6634417170174443520_n.jpg?_nc_cat=1&ccb=2&_nc_sid=1eb0c7&_nc_ohc=2te5GJq43rIAX_jW4dZ&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=0fd2b20c16b95924c276a8a279366bfe&oe=6041E991',
+  }],
+  lastMessage: {
+    id: 'm1',
+    content: 'Well done this sprint, guys!',
+    createdAt: '2020-10-03T14:48:00.000Z',
+  }
+}, {
+  id: '2',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }, {
+    id: 'u3',
+    name: 'Daniil',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }],
+  lastMessage: {
+    id: 'm2',
+    content: 'How are you doing?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '3',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }, {
+    id: 'u4',
+    name: 'Alex',
+    imageUri: 'https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-1/c0.0.130.130a/p130x130/116115891_160967528845475_407458543028203472_n.jpg?_nc_cat=106&ccb=2&_nc_sid=dbb9e7&_nc_ohc=bHekSZvVk34AX870SJ_&_nc_ht=scontent.fdac5-1.fna&tp=27&oh=4f6b71397321456f9f4d7650f6f6da40&oe=6042C1B5',
+  }],
+  lastMessage: {
+    id: 'm3',
+    content: 'Hi, Vadim.',
+    createdAt: '2020-10-02T14:48:00.000Z',
+  }
+}, {
+  id: '4',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }, {
+    id: 'u5',
+    name: 'Vlad',
+    imageUri: 'https://scontent.fdac5-2.fna.fbcdn.net/v/t1.0-1/p130x130/133130919_446837376719544_3470520172657845319_n.jpg?_nc_cat=104&ccb=2&_nc_sid=dbb9e7&_nc_ohc=rkOOupsRX7MAX-2zXl1&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=d89bb0f678c89a44adac334e600c78a9&oe=6042A412',
+  }],
+  lastMessage: {
+    id: 'm4',
+    content: 'Can you review my last merge',
+    createdAt: '2020-09-29T14:48:00.000Z',
+  }
+}, {
+  id: '5',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+  }, {
+    id: 'u6',
+    name: 'Elon Musk',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t1.0-9/117929133_107809907710190_7419805747453745280_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_eui2=AeHyr9m8dMUXCyqgYiDxTTMqypeilYrkr1_Kl6KViuSvX2aKglh0TaInpI6Uqbk8nRSKq6iOQuTncbpb6Uik2iH8&_nc_ohc=YusbV4P7yQMAX9ptPAN&_nc_ht=scontent.fkiv3-1.fna&oh=ebee2db09b2a70c9dbef1bdad876c09c&oe=5F9D4112',
+  }],
+  lastMessage: {
+    id: 'm5',
+    content: 'I would be happy',
+    createdAt: '2020-09-30T14:48:00.000Z',
+  }
+}, {
+  id: '6',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+  }, {
+    id: 'u7',
+    name: 'Adrian',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-8/12185020_10206884996416284_5116038671917824834_o.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeGr_NhJtwGWWfHbkoBSmifLKzIg47DpEF0rMiDjsOkQXf7yavDd4m-3CLbl5nqAMGcj2nn0Dqg7beNAzuZMzTFK&_nc_ohc=KOCn_AYCJxkAX_ZcLBP&_nc_ht=scontent.fkiv3-1.fna&oh=8cc4e5445ce4633db9d6234f42ccb368&oe=5F9C24D1',
+  }],
+  lastMessage: {
+    id: 'm6',
+    content: 'I have a solution',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '7',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+  }, {
+    id: 'u8',
+    name: 'Borja',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t1.0-9/14639688_10154618563394501_7732414502546927586_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_eui2=AeHoAmU6vBHtxse4pY5lCxy5mE72isysx5eYTvaKzKzHl0wF5HhppdSbpFTEnGVXBMLig16R9B4iHrsuAOk_V_fY&_nc_ohc=Sit1NLT0GMsAX9baI56&_nc_ht=scontent.fkiv3-1.fna&oh=16d88a54bb1629c24e77afed0c49f869&oe=5F9EAC05',
+  }],
+  lastMessage: {
+    id: 'm7',
+    content: 'How are you doing?',
+    createdAt: '2020-10-02T15:40:00.000Z',
+  }
+}, {
+  id: '8',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+  }, {
+    id: 'u9',
+    name: 'Mom',
+    imageUri: 'https://image.shutterstock.com/image-vector/super-mom-hero-superhero-cartoon-600w-720015928.jpg',
+  }],
+  lastMessage: {
+    id: 'm8',
+    content: 'Dear, did you eat?',
+    createdAt: '2020-09-27T15:40:00.000Z',
+  }
+}, {
+  id: '9',
+  users: [{
+    id: 'u1',
+    name: 'Vadim',
+    imageUri: 'https://scontent.fkiv3-1.fna.fbcdn.net/v/t31.0-1/s200x200/22256588_1932617800312085_5686197942193420542_o.jpg?_nc_cat=110&_nc_sid=7206a8&_nc_eui2=AeF3UwtnAs3QLEJRnLSp4-hQxlokCBJZ6JPGWiQIElnok9HafHyjqv9D4bW9zeNFfNJlg5jLsvbewM7j5OD-OFy-&_nc_ohc=IxycgYSpqQEAX8EcTqI&_nc_ht=scontent.fkiv3-1.fna&tp=7&oh=640a83293bb75378958d22b633302f1b&oe=5F9F4BB7',
+  }, {
+    id: 'u10',
+    name: 'Angelina Jolie',
+    imageUri: 'https://lkbkspro.s3.amazonaws.com/atelier-management/gs_58d933b8-98b4-468e-b229-43100a9620a7.jpg',
+  }],
+  lastMessage: {
+    id: 'm9',
+    content: 'Meet me at the same place',
+    createdAt: '2020-09-25T15:40:00.000Z',
+  },
+}]
