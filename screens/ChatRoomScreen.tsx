b8cb2402b80571efe23493ad7bdbbf3c4febbcbf
Mahfuzur Rahman <imahfuzurx@gmail.com>
2021-02-06 09:18:23 +0600

    implementing design

---
+++ screens/ChatRoomScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Text} from "react-native";
+
+const ChatRoomScreen = () => {
+    return (
+        <Text>Chat Room</Text>
+    )
+}
+
+export default ChatRoomScreen;
